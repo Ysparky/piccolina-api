2eeac1dbaefb4cea74184a948f103021b5894646
Jose Caldas <jscr272000.jscr@gmail.com>
2021-11-29 05:24:41 -0500

    chore: main configs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
-import { NestFactory } from '@nestjs/core';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+
+  app.enableCors();
+
+  app.setGlobalPrefix('api');
   await app.listen(process.env.PORT);
 }
 bootstrap();
